38f40708a06e26026543e696d0c82ca44b7de074
=notSinhaSid <deadlysinhasiddhartha2000@gmail.com>
2025-04-30 16:56:20 +0530

    working on vite.config.js

---
+++ projectOld13/resources/js/about.js
@@ -0,0 +1,1 @@
+alert('This is js file included in file');
---
+++ projectOld13/resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>contact</title>
+    {{-- <link rel="stylesheet" href="css/contact.css"> --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/contact.css') }}"> --}}
+    <link rel="stylesheet" href="{{ url('css/contact.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>heading inside col inside row inside container</h1>
+            </div>
+        </div>
+    </div>
+
+<script src="js/contact.js"></script>
+</body>
+</html>
