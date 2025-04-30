6bee2724cf08e179a5fcd67018c583f3ce464fd9
=notSinhaSid <deadlysinhasiddhartha2000@gmail.com>
2025-04-30 11:28:17 +0530

    layour using class based components

---
+++ projectOld11/resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot name="title">
+        Contact Us
+    </x-slot>
+</x-layout>
---
+++ projectOld11/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot name="title">
+        Home
+    </x-slot>
+    <h3>HOME Page</h3>
+    <x-slot name="banner">
+        <span>This is banner section</span>
+    </x-slot>
+</x-layout>
